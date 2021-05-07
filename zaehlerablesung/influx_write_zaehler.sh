a6ec13c513fb0f1604194d90ae6c645baf9091c2
Georg Russ <datacyclist@georgruss.ch>
2021-05-07 21:31:49 +0200

    Script, um Zaehlerdaten nach InfluxDB cloud zu loggen

---
+++ zaehlerablesung/influx_write_zaehler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INFLUX_TOKEN=`cat influx_token`
+
+# Wenn das Script aufgerufen wird, wird ein Datenpunkt nach InfluxDB geschrieben.
+# Der Datenpunkt entspricht 1Wh verbrauchter Energie.
+
+curl -i -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
+	  --header "Authorization: Token $INFLUX_TOKEN"\
+          --data-raw "
+	  stromzaehler_1wh,host=smartmeter value=1
+	  " 
