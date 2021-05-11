0e9a5187bbf1c4a4c20d2173580ce1a7e1bcf923
Georg Russ <datacyclist@georgruss.ch>
2021-05-11 23:37:37 +0200

    delay fuer curl eingebaut wegen thingspeak-GET-limit

--- sensors/log_sensoren_to_influx.sh
+++ sensors/log_sensoren_to_influx.sh
@@ -2,6 +2,7 @@
 
 # influx token in File ablegen, wird nicht mit abgelegt im GIT
 INFLUX_TOKEN=`cat influx_token`
+API_KEY_TS=`cat thingspeak_apikey`
 
 # Daten von Heizung abholen, alles auf einmal :-)
 #MESSWERTE=`/usr/local/bin/vclient -m -c 'getTempWWist,getTempWWsoll,getTempKist,getTempAged,getTempA,\
@@ -39,7 +40,35 @@ MESS=`echo $MESSWERTE | sed 's/ airpressure/\nairpressure/g;s/ basement/\nbaseme
 # Daten in influxdb POSTen
 curl -s -i -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
 	          --header "Authorization: Token $INFLUX_TOKEN"\
-            --data-raw "
-	 $MESS
-	            "
+            --data-raw "$MESS "
 
+# Daten auch nach Thingspeak loggen
+# GET https://api.thingspeak.com/update?api_key=XY8HCUA6HPMCWIHG&field1=0
+
+##############################
+# Daten auch noch nach thingspeak loggen
+##############################
+
+# vorher 20s warten -- thingspeak erlaubt nur updates alle 15s und der host
+# raspberryiot sendet auch minütlich
+
+sleep 30s
+
+#echo $MESSWERTEAUSSEN
+TEMPERATUR_SUEDSEITE=`echo $MESSWERTEAUSSEN | sed 's/.*temperature_sth\svalue=\(-*[0-9][0-9].[0-9]\).*air.*/\1/'`
+echo $TEMPERATUR_SUEDSEITE
+
+LUFTDRUCK=`echo $MESSWERTEAUSSEN | sed 's/.*airpressure\svalue=\([0-9]*[0-9][0-9][0-9].[0-9]\).*/\1/'`
+echo $LUFTDRUCK
+
+#TEMPERATUR_WARMWASSER=`echo $MESSWERTE | sed 's/.*getTempWWist.value\s\(-*[0-9][0-9].[0-9]\).*getTempWWsoll.*/\1/'`
+#echo $TEMPERATUR_NORDSEITE
+
+#TEMPERATUR_HWR=`cat /var/tmp/bmp280_temperature`
+
+curl -X GET -G https://api.thingspeak.com/update \
+ 	-d "api_key=$API_KEY_TS" \
+ 	-d "field4=$TEMPERATUR_SUEDSEITE" \
+ 	-d "field5=$LUFTDRUCK" \
+     	--header "Content-type: application/x-www-form-urlencoded" \
+  	--header "Accept: text/plain"
