609c4cc9fbe1828f6a119106cb7b0d058ecad704
Georg Russ <datacyclist@georgruss.ch>
2021-05-11 18:05:11 +0200

    query influxDB completed

--- lcd/query_influx.sh
+++ lcd/query_influx.sh
@@ -3,38 +3,32 @@
 # influx token in File ablegen, wird nicht mit abgelegt im GIT
 INFLUX_TOKEN=`cat influx_token_read`
 
-# daten von InfluxDB holen
-#curl -s -i -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
-#	          --header "Authorization: Token $INFLUX_TOKEN"\
-#            --data-raw "
-#	 $MESS
-#	            "
+CSVDATA=`curl -s --request POST \
+ "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/query?org=influx@georgruss.ch"\
+  --header "Authorization: Token $INFLUX_TOKEN"\
+	--header "Accept: application/csv" \
+	--header "Content-type: application/vnd.flux" \
+	--data '
+   from(bucket: "od10_messwerte")
+	  |> range(start: -1m)
+    |> filter(fn: (r) => r["_measurement"] == "getTempWWist" or r["_measurement"] == "getTempVListM1" or r["_measurement"] == "getTempKist" or r["_measurement"] == "getTempAbgas")
+	  |> filter(fn: (r) => r["_field"] == "value")
+		|> aggregateWindow(every: 30s, fn: last, createEmpty: false)
+		|> yield(name: "last")
+		'
+		`
 
-# Daten von Heizung abholen, alles auf einmal :-)
-# MESSWERTE=`/usr/local/bin/vclient -m -c 'getTempWWist,getTempWWsoll,getTempKist,getTempAged,getTempA,\
-# getVolStrom,getBrennerStatus,getBrennerStarts,getBrennerStunden1,getLeistungIst,\
-# getPumpeStatusM1,getPumpeDrehzahlIntern,getBetriebArt,getTempVListM1,getTempVLsollM1,getTempRL17A,getTempAbgas '`
+echo "$CSVDATA"
+#echo $CSVDATA | awk -F "," '{ print $2 }'
+#echo "$CSVDATA" | awk -F "," '{ print $7 }'
+#echo "$CSVDATA" | awk -v i=2 -v j=3 'NR == i {print $j}'
+#echo "$CSVDATA" | awk 'NR == 2' | awk -F "," '{ print $7 }'
+ABGASTEMP=`echo "$CSVDATA" | awk /getTempAbgas/ | awk -F "," '{ print $7 }'`
+KESSELTEMP=`echo "$CSVDATA" | awk /getTempKist/ | awk -F "," '{ print $7 }'`
+VORLAUFTEMP=`echo "$CSVDATA" | awk /getTempVListM1/ | awk -F "," '{ print $7 }'`
+WWTEMP=`echo "$CSVDATA" | awk /getTempWWist/ | awk -F "," '{ print $7 }'`
 
-#echo $MESSWERTE
-
-# umformatieren
-
-# vorher
-# getTempAged.value 8.500000
-
-# nachher
-# getTempAged value=8.500000
-
-# Messwertzeilen durch \n getrennt
-
-MESS=`echo $MESSWERTE | sed 's/ get/\nget/g;s/.value\ / value=/g' `
-
-# such a mess...
-
-# Daten in influxdb POSTen
-#curl -s -i -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
-#	          --header "Authorization: Token $INFLUX_TOKEN"\
-#            --data-raw "
-#	 $MESS
-#	            "
-
+echo $ABGASTEMP >> /var/tmp/abgastemp
+echo $KESSELTEMP >> /var/tmp/kesseltemp
+echo $VORLAUFTEMP >> /var/tmp/vorlauftemp
+echo $WWTEMP >> /var/tmp/wwtemp
