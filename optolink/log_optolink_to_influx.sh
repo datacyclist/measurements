c13db3401c04a403c28212370499b882df2af67a
Georg Russ <git@georgruss.ch>
2021-05-14 16:58:27 +0200

    Heizungsdaten auch in File ablegen

--- optolink/log_optolink_to_influx.sh
+++ optolink/log_optolink_to_influx.sh
@@ -7,12 +7,13 @@ INFLUX_TOKEN=`cat influx_token`
 API_KEY_TS=`cat thingspeak_apikey`
 #echo $API_KEY_TS
 
+TIMESTAMP=`date +"%s"`
+FILEDATE=`date +"%Y%m%d"`
+
 # Daten von Heizung abholen, alles auf einmal :-)
 MESSWERTE=`/usr/local/bin/vclient -m -c 'getTempWWist,getTempWWsoll,getTempKist,getTempAged,getTempA,\
 getVolStrom,getBrennerStatus,getBrennerStarts,getBrennerStunden1,getLeistungIst,\
 getPumpeStatusM1,getPumpeDrehzahlIntern,getBetriebArt,getTempVListM1,getTempVLsollM1,getTempRL17A,getTempAbgas '`
-
-#echo $MESSWERTE
 
 # umformatieren
 
@@ -28,13 +29,24 @@ MESS=`echo $MESSWERTE | sed 's/ get/\nget/g;s/.value\ / value=/g' `
 
 # such a mess...
 
+##############################
 # Daten in influxdb POSTen
+##############################
+
 curl -s -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
 	          --header "Authorization: Token $INFLUX_TOKEN"\
             --data-raw "$MESS"
 
 ##############################
-# Daten nach thingspeak loggen
+# Daten in File ablegen zwecks Auswertung in R
+##############################
+LOGLINE=$TIMESTAMP" "$MESSWERTE
+
+#echo $MESSWERTE >> ~/mnt/nas/zaehlerlog/gastherme/$DATE-optolinklog.csv
+echo $LOGLINE >> ~/mnt/nas/zaehlerlog/gastherme/$FILEDATE-optolinklog.csv
+
+##############################
+# einzelne Messwerte nach thingspeak loggen
 ##############################
 
 TEMPERATUR_NORDSEITE=`echo $MESSWERTE | sed 's/.*getTempA.value\s\(-*[0-9]*[0-9].[0-9]\).*getVol.*/\1/'`
