81ef5e3c55e88d6b09760b021b63e11e56fbca68
Georg Russ <datacyclist@georgruss.ch>
2021-10-22 20:29:43 +0200

    Zählerstände Wasser werden per Quartalsablesung korrigiert

--- script/23-calculate-wasser-zaehlerstaende.R
+++ script/23-calculate-wasser-zaehlerstaende.R
@@ -3,7 +3,7 @@
 #
 # liegen in CSV-Files, eins pro Tag, pro Zeile ein Timestamp und pro Zeile eine Wh
 #
-# 20210730, Georg Russ
+# 20211022, Georg Russ
 ##############################
 
 library(tidyverse)
@@ -17,8 +17,6 @@ library(reshape2)
 library(readr)
 
 source("theme-verbrauch.R")
-#source("05-read-googlesheet.R")
-#source("08-grundpreise.R")
 
 # für millisekundengenaue Berechnungen Anzahl Nachkommastellen anpassen
 options("digits.secs"=6)
@@ -28,16 +26,32 @@ filedateprefix <- format(Sys.time(), "%Y%m%d")
 figdirprefix <- '../figs/'
 cachedirprefix <- '../cache/'
 
-#vortag <- format(Sys.time()-days(1), "%Y%m%d")
-#dat_gestern <- read_csv(file=paste("/home/russ/mnt/nas/zaehlerlog/", vortag, '-stromzaehler-ping.csv', sep=""),
-#								col_names=FALSE)
+# Daten zur Quartalsabrechnung holen, dort manuelle Eintragung 4x im Jahr zum
+# Korrigieren von Ablesefehlern -- dieser Data Frame wird später per Join an
+# den Ablesewert-DF gehängt, um maschinelle Ablesefehler mit den offiziellen
+# Ablesewerten zu korrigieren
+
+dfquartal <- read_tsv("../csv/quartalsabrechnung.csv",
+											comment="#") %>%
+	mutate(
+				 quartal = quarter(date),
+				 jahr = year(date),
+				 jq = paste(jahr,"Q",quartal, sep=""),
+				 endstand_quartal = wasser,
+				 verbrauch_quartal = wasser-lead(wasser),
+				 endstand_vorquartal = lead(wasser)
+				 ) %>%
+	select(jq, endstand_quartal, verbrauch_quartal,endstand_vorquartal)
 
 ## Daten vom NAS holen, alle CSVs in einem bestimmten Verzeichnis
+# (man könnte später auch diese CSVs löschen, wenn die Tagesstände einmal
+# berechnet und abgelegt sind)
+
 dat <- list.files(path="/home/russ/mnt/nas/zaehlerlog/", pattern="*wasserzaehler-ping.csv", full.names=TRUE) %>%
 			map_df(~read_csv(.,col_names=FALSE))
 
-# Zählerstände werden per Offset (aus abgelesenen Werten an bestimmtem Datum)
-# und cumsum errechnet
+# Zählerstände werden per Offset (aus offiziellen Quartalsabrechnungen) und
+# cumsum errechnet
 
 df_m3 <- dat %>%
 				mutate(
@@ -48,22 +62,43 @@ df_m3 <- dat %>%
 				) %>%
 	filter(timestamp>=format(as.Date("20210501", "%Y%m%d"))) %>%
 	group_by(datum) %>%
+	# bestimme Verbrauch pro Tag und stelle Join-Key her für Quartalswerte
 	summarise(
 						datum = unique(datum),
-						l = sum(l)
+						l = sum(l),
+				 		quartal = quarter(datum),
+				 		jahr = year(datum),
+				 		jq = paste(jahr,"Q",quartal, sep="")
 						) %>%
 	ungroup() %>%
-	#pivot_wider(names_from=ht_nt, values_from = Wh) %>%
-	#replace_na(list(HT=0)) %>%
+	left_join(dfquartal, by="jq") %>%
 	arrange(datum) %>%
+	# zuerst einfach nur alle Zählerstand-Pings kumulieren, korrigiert wird später
 	mutate(
-				 # 882 Liter am 19.06. sind komisch --> Korrektur mit Quartalsabrechnung
-				 korrektur_q2_2021 = ifelse((datum>=format(as.Date("20210619", "%Y%m%d"))), 1, 0),
-				 m3_cum = round(cumsum(l)/1000+376.900 - korrektur_q2_2021*0.8, digits=2)
-				 #HT_stand_cum = cumsum(HT)/1000 + 4666,
-				# NT_stand_cum = cumsum(NT)/1000 + 9269
+				 m3_cum = round(cumsum(l)/1000, digits=2),
 				 ) %>%
+	# Korrektur erfolgt pro Quartal mit Hilfe der Werte aus der offiziellen Quartalsabrechnung
+	group_by(jq) %>%
+	mutate(
+				 # bestimme die selber per Zählerstand-Ping ermittelte Menge
+				 min_m3_quartal = min(m3_cum),
+				 max_m3_quartal = max(m3_cum),
+				 ping_m3 = max_m3_quartal-min_m3_quartal,
+
+				 # bestimme den Korrekturfaktor (pro Quartal)
+				 korrekturfaktor_quartal = verbrauch_quartal/ping_m3,
+
+				 # Korrekturfaktor fürs aktuelle noch nicht beendete Quartal ist 1
+				 korrekturfaktor_quartal = ifelse(is.na(korrekturfaktor_quartal),1,korrekturfaktor_quartal),
+
+				 # korrigiere jeden einzelnen Tageswert
+				 l_korrigiert = l*korrekturfaktor_quartal,
+
+				 # berechne daraus den korrigierten Zählerstand
+				 m3_cum_korrigiert = round(cumsum(l_korrigiert)/1000+endstand_vorquartal, digits=2)
+				 ) %>%
+	ungroup() %>%
 	arrange(desc(datum)) %>%
-	select(datum, m3_cum) %>%
+	select(datum, m3_cum_korrigiert, m3_cum) %>%
   write_tsv(file=paste(cachedirprefix, filedateprefix, "-zaehlerstande-wasser_errechnet.csv", sep=""))
 
