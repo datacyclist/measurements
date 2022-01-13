23ffb76fa17dffa7ed2320cb657b7dd64a0acc38
Georg Russ <datacyclist@georgruss.ch>
2022-01-13 13:16:51 +0100

    Zeilenumbruch entfernt

--- zaehlerablesung/influx_write_zaehler.sh
+++ zaehlerablesung/influx_write_zaehler.sh
@@ -7,6 +7,4 @@ INFLUX_TOKEN=`cat /home/russ/bin/measurements/zaehlerablesung/influx_token`
 
 curl -i -XPOST "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=influx@georgruss.ch&bucket=od10_messwerte&precision=ms"\
 	  --header "Authorization: Token $INFLUX_TOKEN"\
-          --data-raw "
-	  stromzaehler_1wh,host=smartmeter value=1
-	  " 
+          --data-raw "stromzaehler_1wh,host=smartmeter value=1" 
