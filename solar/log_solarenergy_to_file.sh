9db097cb0c1c115d00baf9cec829b5652f7bacb6
Georg Russ <datacyclist@georgruss.ch>
2022-04-13 11:50:58 +0200

    Energie vom Solarpanel loggen

---
+++ solar/log_solarenergy_to_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script läuft per cron auf host smartmeter (oder auf einem der anderen raspis)
+
+# Daten von powermeter-solar im Keller holen
+solarenergy_panel0_today=`curl -s -X GET http://192.168.0.78/cm?cmnd=Status%208 | jq -r '.StatusSNS.ENERGY.Today'`
+#echo $solarenergy_panel0_today
+dt=$(date '+%Y-%m-%d_%H:%M:%S')
+
+# direkt aufs NAS in ein File loggen
+echo $dt $solarenergy_panel0_today >> ~/mnt/nas/zaehlerlog/solar/solar_panel0.log
