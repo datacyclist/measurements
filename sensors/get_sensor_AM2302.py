6af52d6725e5f31f5535aad5b02aa4ef97f4fb81
Georg Russ <git@georgruss.ch>
2022-07-03 14:43:21 +0200

    AM2302 sensor on raspberry pi and service file

--- sensors/get_sensor_AM2302.py
+++ sensors/get_sensor_AM2302.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# this is an endless loop to write the sensor readings to a file in /var/log (on tmpfs)
+
+# it will be started in a service file am2302.service for systemctl
+
 import sys
 import Adafruit_DHT
 #import Adafruit_BMP.BMP085 as BMP085
@@ -9,14 +13,18 @@ import os
 from datetime import datetime
 from time import gmtime,strftime
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 15)
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 18)
 
-if humidity is not None and temperature is not None and float(humidity)<105 :
-    #print(temperature)
-    humishort = str(round(float(humidity),1))
-    tempshort = str(round(float(temperature),1))
-    print('humidity_estrich value=' + humishort)
-    print('temperature_estrich value=' + tempshort)
-    #print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    sys.exit(1)
+    if humidity is not None and temperature is not None and float(humidity)<105 :
+        humishort = str(round(float(humidity),1))
+        tempshort = str(round(float(temperature),1))
+        ftempbmp = open('/var/log/am2302_temperature', 'w')	
+        ftempbmp.write(tempshort)
+        ftempbmp.close()
+        ftempbmp = open('/var/log/am2302_humidity', 'w')	
+        ftempbmp.write(humishort)
+        ftempbmp.close()
+        time.sleep(10)
+    else:
+        time.sleep(10)
