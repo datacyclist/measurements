423eb67e89b1f0e0c7f54de145d40b5b70b7782f
Georg Russ <git@georgruss.ch>
2022-07-05 20:54:24 +0200

    error handling on empty files

--- eink/write_epaper.py
+++ eink/write_epaper.py
@@ -12,100 +12,151 @@ logging.basicConfig(level=logging.INFO)
 
 # dimensions: 296x152 px
 
-try:
+#print(ac1)
+#if os.path.getsize('/var/log/AC_kWh') == 0:
+##if len(file1.read()) == 0:
+#    AC_kWh="0"
+#else:
+#    AC_kWh=float(file1.read())
+#file1.close()                                                                                                
+#print(AC_kWh)
+
+#file1 = open('/var/log/AC_kWh','r')                                                                     
+#ac1 = float(file1.read())
+#print(file1.read())
+#print(len(file1.read()))
+#ac1 = file1.read()).strip()
+#print(ac1)
+#file1 = open('/var/log/AC_kWh','r')                                                                     
+#print(file1.read())
+#print(len(file1.read()))
+#if len(file1.read())==0:
+#    AC_kWh="0"
+#else:
+#AC_kWh=str(round(ac1,2))
+#file1.close()                                                                                                
+
+#time.sleep(5)
+
+#try:
     # logging.info("epd2in66 Demo")
     
     # epd = epd2in66.EPD()
-    epd = epaper.epaper('epd2in66').EPD()
-    #logging.info("init and Clear")
-    epd.init(0) # =full refresh
-    epd.Clear()
-    # logging.info(epd.height)
-    # logging.info(epd.width)
+epd = epaper.epaper('epd2in66').EPD()
+#logging.info("init and Clear")
+epd.init(0) # =full refresh
+epd.Clear()
+# logging.info(epd.height)
+# logging.info(epd.width)
 
-    font24 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 24)
-    font18 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 18)
-    font14 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 14)
+font24 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 24)
+font18 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 18)
+font14 = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/verdana.ttf", 14)
 
-    
-    # Drawing vertically
-    #logging.info("4.Drawing on the Vertical image...")
-    Limage = Image.new('1', (epd.width, epd.height), 0xFF)  # 0xFF: clear the frame
-    # draw = ImageDraw.Draw(Limage)
-    # draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    # draw.text((2, 20), '2.66inch epd', font = font18, fill = 0)
-    # draw.text((10, 40), u'微雪电子', font = font24, fill = 0)
-    # draw.line((10, 90, 60, 140), fill = 0)
-    # draw.line((60, 90, 10, 140), fill = 0)
-    # draw.rectangle((10, 90, 60, 140), outline = 0)
-    # draw.line((95, 90, 95, 140), fill = 0)
-    # draw.line((70, 115, 120, 115), fill = 0)
-    # draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    # draw.rectangle((10, 150, 60, 200), fill = 0)
-    # draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    # epd.display(epd.getbuffer(Limage))
-    # time.sleep(5)
+# Drawing vertically
+#logging.info("4.Drawing on the Vertical image...")
+# draw = ImageDraw.Draw(Limage)
+# draw.text((2, 0), 'hello world', font = font18, fill = 0)
+# draw.text((2, 20), '2.66inch epd', font = font18, fill = 0)
+# draw.text((10, 40), u'微雪电子', font = font24, fill = 0)
+# draw.line((10, 90, 60, 140), fill = 0)
+# draw.line((60, 90, 10, 140), fill = 0)
+# draw.rectangle((10, 90, 60, 140), outline = 0)
+# draw.line((95, 90, 95, 140), fill = 0)
+# draw.line((70, 115, 120, 115), fill = 0)
+# draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
+# draw.rectangle((10, 150, 60, 200), fill = 0)
+# draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
+# epd.display(epd.getbuffer(Limage))
 
+Himage = Image.new('1', (epd.height, epd.width), 0xFF)  # 0xFF: clear the frame
+draw = ImageDraw.Draw(Himage)
+draw.text((10, 0), 'System Startup, waiting... ', font = font18, fill = 0)
+epd.display(epd.getbuffer(Himage))
 
-    # partial update, mode 1
-    epd.init(1) # = partial refresh mode
-    epd.Clear()
-    time_draw = ImageDraw.Draw(Limage)
-    num = 0
+# wait after machine startup
+time.sleep(15)
 
-    while (True):
+epd.init(0) # =full refresh
+epd.Clear()
 
-        # get data
-        timedate = time.strftime("%d.%m.%Y %H:%M:%S")
-        #print(timedate)
+Limage = Image.new('1', (epd.width, epd.height), 0xFF)  # 0xFF: clear the frame
+# partial update, mode 1
+epd.init(1) # = partial refresh mode
+epd.Clear()
+time_draw = ImageDraw.Draw(Limage)
+num = 0
+
+while (True):
+
+    # get data
+    timedate = time.strftime("%d.%m.%Y %H:%M:%S")
+
+    # Raumtemperatur Buero
+
+    if os.path.getsize('/var/log/am2302_temperature') == 1:
+        tempbuero="NA"
+    else:
+        file1 = open('/var/log/am2302_temperature','r')
+        tempbuero=str(round(float(file1.read()),1))
+        file1.close()                                                                                                
+
+    #ftempbuero = open('/var/log/am2302_temperature','r')                                                                     
+    #tempbuero = str(round(float(ftempbuero.read()),1))
+
+    # Luftfeuchte Buero
+    if os.path.getsize('/var/log/am2302_humidity') == 1:
+        tempbuero="NA"
+    else:
+        file1 = open('/var/log/am2302_humidity','r')
+        feuchtebuero=str(round(float(file1.read()),1))
+        file1.close()                                                                                                
         
-        # Raumtemperatur Buero
-        ftempbuero = open('/var/log/am2302_temperature','r')                                                                     
-        tempbuero = str(round(float(ftempbuero.read()),1))
-        ftempbuero.close()                                                                                                
-        # Luftfeuchte Buero
-        ffeuchtebuero = open('/var/log/am2302_humidity','r')                                                                     
-        feuchtebuero = str(round(float(ffeuchtebuero.read()),1))
-        ffeuchtebuero.close()                                                                                                
-        # Aussentemperatur
-        file1 = open('/var/log/aussentemperatur','r')                                                                     
-        aussentemperatur = str(round(float(file1.read()),1))
-        file1.close()                                                                                                
-        # Verbrauch A/C
-        file1 = open('/var/log/AC_kWh','r')                                                                     
-        AC_kWh= str(round(float(file1.read()),2))
-        file1.close()                                                                                                
-        # Netzbezug Wh
-        file1 = open('/var/log/netzbezug_Wh','r')                                                                     
-        netzbezug_Wh = str(round(float(file1.read())/1000,2))
-        file1.close()                                                                                                
-        # Solar-Leistung aktuell
-        file1 = open('/var/log/solar_W','r')                                                                     
-        solar_W = str(int(file1.read()))
-        file1.close()                                                                                                
-        # Solar-Erzeugung heute
-        file1 = open('/var/log/solar_kWh','r')                                                                     
-        solar_kWh = str(float(file1.read()))
+    # Aussentemperatur
+    file1 = open('/var/log/aussentemperatur','r')                                                                     
+    aussentemperatur = str(round(float(file1.read()),1))
+    file1.close()                                                                                                
+    
+    # Verbrauch A/C
+    
+    if os.path.getsize('/var/log/AC_kWh') == 1:
+        AC_kWh="NA"
+    else:
+        file1 = open('/var/log/AC_kWh','r')
+        AC_kWh=str(round(float(file1.read()),2))
         file1.close()                                                                                                
 
-        # this triggers partial update for the whole specified area (=the whole display here)
-        time_draw.rectangle((0, 0, 152, 296), fill = 255)
-        #time_draw.text((0, 10), time.strftime('%H:%M:%S'), font = font24, fill = 0)
-        time_draw.text((0, 00), timedate, font = font14, fill = 0)
-        time_draw.line((0, 22, 152, 22), fill = 0)
-        time_draw.text((0, 22), "aktuell", font = font14, fill = 0)
-        time_draw.line((0, 37, 152, 37), fill = 0)
-        time_draw.text((0, 40), tempbuero+"°C Büro", font = font18, fill = 0)
-        time_draw.text((0, 60), feuchtebuero+"% Büro", font = font18, fill = 0)
-        time_draw.text((0, 80), aussentemperatur+"°C aussen", font = font18, fill = 0)
-        time_draw.text((0, 100), solar_W+" W Solar", font = font18, fill = 0)
-        time_draw.line((0, 125, 152, 125), fill = 0)
-        time_draw.text((0, 125), "Mengen heute", font=font14, fill = 0)
-        time_draw.line((0, 140, 152, 140), fill = 0)
-        time_draw.text((0, 145), AC_kWh+" kWh A/C", font = font18, fill = 0)
-        time_draw.text((0, 165), solar_kWh+" kWh Solar", font = font18, fill = 0)
-        time_draw.text((0, 185), netzbezug_Wh+" kWh Netz", font = font18, fill = 0)
-        epd.display(epd.getbuffer(Limage))
+    # Netzbezug Wh
+    file1 = open('/var/log/netzbezug_Wh','r')                                                                     
+    netzbezug_Wh = str(round(float(file1.read())/1000,2))
+    file1.close()                                                                                                
+    # Solar-Leistung aktuell
+    file1 = open('/var/log/solar_W','r')                                                                     
+    solar_W = str(int(file1.read()))
+    file1.close()                                                                                                
+    # Solar-Erzeugung heute
+    file1 = open('/var/log/solar_kWh','r')                                                                     
+    solar_kWh = str(float(file1.read()))
+    file1.close()                                                                                                
+    
+    # this triggers partial update for the whole specified area (=the whole display here)
+    time_draw.rectangle((0, 0, 152, 296), fill = 255)
+    #time_draw.text((0, 10), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+    time_draw.text((0, 00), timedate, font = font14, fill = 0)
+    time_draw.line((0, 22, 152, 22), fill = 0)
+    time_draw.text((0, 22), "aktuell", font = font14, fill = 0)
+    time_draw.line((0, 37, 152, 37), fill = 0)
+    time_draw.text((0, 40), tempbuero+"°C Büro", font = font18, fill = 0)
+    time_draw.text((0, 60), feuchtebuero+"% Büro", font = font18, fill = 0)
+    time_draw.text((0, 80), aussentemperatur+"°C aussen", font = font18, fill = 0)
+    time_draw.text((0, 100), solar_W+" W Solar", font = font18, fill = 0)
+    time_draw.line((0, 125, 152, 125), fill = 0)
+    time_draw.text((0, 125), "Mengen heute", font=font14, fill = 0)
+    time_draw.line((0, 140, 152, 140), fill = 0)
+    time_draw.text((0, 145), AC_kWh+" kWh A/C", font = font18, fill = 0)
+    time_draw.text((0, 165), solar_kWh+" kWh Solar", font = font18, fill = 0)
+    time_draw.text((0, 185), netzbezug_Wh+" kWh Netz", font = font18, fill = 0)
+    epd.display(epd.getbuffer(Limage))
         
         #num = num + 1
         #if(num == 5):
@@ -154,11 +205,3 @@ try:
     # 
     # logging.info("Goto Sleep...")
     # epd.sleep()
-    
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd2in66.epdconfig.module_exit()
-    exit()
