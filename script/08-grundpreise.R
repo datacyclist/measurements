282db31efeb7922998958e970b78dac9f4068ce4
Georg Russ <github@georgruss.ch>
2022-08-30 16:08:11 +0200

    Stromtarife 2023 updated

--- script/08-grundpreise.R
+++ script/08-grundpreise.R
@@ -5,13 +5,32 @@
 # Eine "Preiszeile" pro Jahr, wird dann später drangejoint an Verbrauchswerte
 # fuer die Kostenberechnung
 
+preise2023 <- data.frame(jahr=2022) %>%
+	mutate(
+												 mwst1 = 0.025, #2.5% Wasser
+												 mwst2 = 0.077, #7.7% Gas/Strom
+												 preis_menge_strom_ht = (16.16+10.66)/(1+mwst2), #Rp. netto 
+												 preis_menge_strom_nt = (12.92+6.89)/(1+mwst2), #Rp.
+												 preis_menge_strom_sdl_kev_abgaben = (0.50+3.75)/(1+mwst2), #Rp.
+												 # Strom HT 31.07 Rp./kWh, Strom NT 24.06 Rp./kWh
+												 preis_menge_gas = (3.70+0.90+0.40+2.169)/(1+mwst2), #Rp., Achtung Nettopreise!
+												 preis_menge_wasser = 1.28/(1+mwst1), #Fr. pro Kubikmeter,
+												 preis_menge_abwasser = 1.44/(1+mwst2), #Fr. pro Kubikmeter,
+												 preis_grund_wasser = 10.25/(1+mwst1), #Fr. pro Monat
+												 preis_grund_gas = 6.46/(1+mwst2), #Fr. pro Monat
+												 preis_grund_strom = 7.00/(1+mwst2), #Fr. pro Monat
+												 preis_grund_abwasser = 4.00/(1+mwst2), #Fr. pro Monat
+												 )
+
 preise2022 <- data.frame(jahr=2022) %>%
 	mutate(
 												 mwst1 = 0.025, #2.5% Wasser
 												 mwst2 = 0.077, #7.7% Gas/Strom
-												 preis_menge_strom_ht = (8.72+10.12)/(1+mwst2), #Rp. netto vorher aus offiziellen Bruttopreisen errechnen!
+												 preis_menge_strom_ht = (8.72+10.12)/(1+mwst2), #Rp. netto (aus Bruttopreis errechnet(
 												 preis_menge_strom_nt = (7.65+6.25)/(1+mwst2), #Rp.
 												 preis_menge_strom_sdl_kev_abgaben = (0.17+3.75)/(1+mwst2), #Rp.
+												 # Strom HT 22.76 Rp./kWh, Strom NT 18.82 Rp./kWh
+												 preis_menge_gas = (3.70+0.90+0.40+2.169)/(1+mwst2), #Rp., Achtung Nettopreise!
 												 preis_menge_gas = (3.70+0.90+0.40+2.169)/(1+mwst2), #Rp., Achtung Nettopreise!
 												 preis_menge_wasser = 1.28/(1+mwst1), #Fr. pro Kubikmeter,
 												 preis_menge_abwasser = 1.44/(1+mwst2), #Fr. pro Kubikmeter,
