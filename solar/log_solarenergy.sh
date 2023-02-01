3a2d3e2f050a051092966304996131920205011d
Georg Russ <datacyclist@georgruss.ch>
2023-02-01 08:21:20 +0100

    more solar panels

--- solar/log_solarenergy.sh
+++ solar/log_solarenergy.sh
@@ -8,6 +8,7 @@ INFLUX_TOKEN=`cat influx_token`
 
 # Daten von powermeter-solar im Keller holen
 solarenergy_panel0_today=`curl -s -X GET http://192.168.0.78/cm?cmnd=Status%208 | jq -r '.StatusSNS.ENERGY.Today'`
+solarenergy_panel12_today=`curl -s -X GET http://192.168.0.80/cm?cmnd=Status%208 | jq -r '.StatusSNS.ENERGY.Today'`
 #echo $solarenergy_panel0_today
 dt=$(date '+%Y-%m-%d_%H:%M:%S')
 
@@ -15,19 +16,19 @@ dt=$(date '+%Y-%m-%d_%H:%M:%S')
 #echo $dt $solarenergy_panel0_today >> ~/mnt/nas/zaehlerlog/solar/solar_panel0.log
 # lokal in ein File loggen, das periodisch (nachts) aufs NAS rsyncen per cron
 echo $dt $solarenergy_panel0_today >> /var/tmp/solar_panel0.log
+echo $dt $solarenergy_panel12_today >> /var/tmp/solar_panel12.log
 #!/bin/bash
 
-SOLARENERGYTODAY=`echo SOLAR_ENERGY_TODAY_kWh value=$solarenergy_panel0_today`
+SOLARENERGYTODAY_PANEL0=`echo SOLAR_ENERGY_TODAY_kWh value=$solarenergy_panel0_today`
+SOLARENERGYTODAY_PANEL12=`echo SOLAR_ENERGY_TODAY_kWh value=$solarenergy_panel12_today`
 
 ## alle Messwerte hintereinander
-MESSWERTE=`echo $SOLARENERGYTODAY`
+MESSWERTE=`echo $SOLARENERGYTODAY_PANEL0 $SOLARENERGYTODAY_PANEL12`
 #
 #echo $MESSWERTE
 
 #### Messwertzeilen durch \n getrennt
 MESS=`echo $MESSWERTE | sed 's/ SOLAR/\nSOLAR/g' `
-
-echo $MESS
 
 # echo $MESS
 
